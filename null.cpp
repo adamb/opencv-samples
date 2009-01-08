f711e9f47eef4f22d49b2b1cb378f5ec73c91ea2
Adam Beguelin <adamb@dao.local>
2009-01-08 10:05:01 -0800

    add some samples

---
+++ null.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <cv.h>
+#include <highgui.h>
+
+int main(int argc, char *argv[])
+{ 
+	
+  IplImage* img = 0; 
+  int height,width,step,channels;
+  uchar *data;
+  int i,j,k;
+
+  if(argc<2){
+    printf("Usage: main <image-file-name>\n");
+    exit(0);
+  }
+
+  // load an image  
+
+}
